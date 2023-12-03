8aee9481d0a945f2bb091db7a0cd1dd16c276330
Leon Klute <leonklute@live.nl>
2023-12-03 05:49:24 +0100

    rustwip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let mut counts = HashMap::new();
+    counts.insert("red", 3);
+    counts.insert("green", 5);
+    counts.insert("blue", 4);
+
+    let mut total = 0;
+    for line in input.lines() {
+        println!("{}", line);
+        let line = line.split_whitespace().collect::<Vec<_>>();
+
+        let mut id = 0;
+        let mut valid = true;
+        for i in (0..line.len()).step_by(2) {
+            if i == 0 {
+                id = line.get(1).unwrap().trim_end_matches(":").parse::<i32>().unwrap();
+            } else {
+                let count = line.get(i).unwrap().parse::<i32>().unwrap();
+                let color = line.get(i + 1).unwrap().trim_matches(|c: char| c.is_ascii_punctuation());
+                let min_count = counts.get(color).unwrap();
+                if count < *min_count {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if valid {
+            total += id;
+        }
+    }
+    println!("{}", total)
 }
