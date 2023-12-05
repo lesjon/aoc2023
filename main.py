334a0fec0880f5a4136fe8b4e65c1d40ea107ac6
Leon Klute <leonklute@live.nl>
2023-12-05 18:37:46 +0100

    succes

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import unittest
 
     
-def d4p1(text: str) -> int:
+def d4p2(text: str) -> int:
     blocks = text.split('\n\n')
     seeds = blocks[0].split()[1:]
     seeds = list(map(int,seeds))
-    print(f'{seeds=}')
+    seed_ranges = []
+    seed_num = 0
+    for i in range(len(seeds)):
+        if i % 2 == 1:
+            continue
+        seed_ranges.append(range(seeds[i], seeds[i]+seeds[i+1]))
+        seed_num  += seeds[i+1]
+
+    del seeds
+
+    print(f'{seed_ranges=} {seed_num=}')
     resource_map = dict()
     for block in blocks[1:]:
         print(f'{block=}')
         lines = block.splitlines()
         from_to, _ = lines[0].split()
         map_from, _, map_to = from_to.split('-')
-        resource_map.update({(map_from, map_to):dict()})
+        resource_map.update({(map_to, map_from):dict()})
         for line in lines[1:]:
             dest_start, src_start, count = line.split()
             dest_start, src_start, count = int(dest_start), int(src_start), int(count)
-            resource_map[(map_from, map_to)].update({range(src_start, src_start + count): (src_start, dest_start)})
+            resource_map[(map_to, map_from)].update({range(dest_start, dest_start + count): (dest_start, src_start)})
         print(resource_map)
     lowest_location = 999999999999999
-    for seed in seeds:
-        value = seed
-        resource = 'seed'
-        while resource != 'location':
+    for i, location in enumerate(range(lowest_location)):
+        if i % 10000 == 0:
+            print(f'test location:{i}')
+        value = location
+        resource = 'location'
+        while resource != 'seed':
             for key in resource_map:
                 if key[0] != resource:
                     continue
@@ -35,9 +47,9 @@ def d4p1(text: str) -> int:
                 except KeyError:
                     pass  # value maps to itself
                 resource = key[1]
-        lowest_location = min(lowest_location, value)
-
-    return lowest_location
+        for seed_range in seed_ranges:
+            if value in seed_range:
+                return location
 
 
 
@@ -45,20 +57,10 @@ def main():
     with open('input.txt', 'r') as f:
         text = f.read()
 
-    print(d4p1(text))
+    print(d4p2(text))
 
 if __name__ == "__main__":
     main()
-# The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
-#
-#     Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-#     Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-#     Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-#     Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
-#
-# So, the lowest location number in this example is 35.
-#
-# What is the lowest location number that corresponds to any of the initial seed numbers?
 
 class Tests(unittest.TestCase):
     text = '''seeds: 79 14 55 13
@@ -96,5 +98,5 @@ humidity-to-location map:
 56 93 4
 '''
     def test(self):
-        self.assertEqual(35, d4p1(self.text))
+        self.assertEqual(46, d4p2(self.text))
 
