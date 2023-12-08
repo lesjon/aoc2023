b8799dce0eeef7769cda0226d3a8eff8e16bb62c
Leon Klute <leonklute@live.nl>
2023-12-08 15:31:56 +0100

    wip2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 
 def parse(node: str) -> tuple[str, dict[str,str]]:
@@ -23,17 +24,33 @@ def main(text: str) -> int:
         if node[0].endswith('A'):
             currents.append(node)
     print(f'path_starts={currents}')
+    z_indices = [[] for _ in currents]
+    seens = [set() for _ in currents]
+    repeats: list[int|None] = [None for _ in currents]
     count = 0
-    while not all(map(lambda n: n[0].endswith('Z'), currents)):
-        if count % 1000 == 0:
-            print(f'{count}')
+    while not all(repeats):
+        print(f'{count}')
         for i in range(len(currents)):
             current = currents[i]
+            print(f'{current=}')
+            if current is None:
+                continue
             lr = route[count % len(route)]
-            count += 1
             next = current[1][lr]
+            if current[0].endswith('Z'):
+                z_indices[i].append(count)
+            state = (count % len(route), current[0])
+            if state in seens[i]:
+                repeats[i] = count
+                currents[i] = None
+            else:
+                seens[i].add(state)
             currents[i] = (next, nodes[next])
-    return count
+        count += 1
+    print(f'{repeats=}')
+    print(f'{z_indices=}')
+    lcm = math.lcm(*map(lambda zs: zs[0], z_indices))
+    return lcm
 
 
 if __name__ == "__main__":
@@ -42,10 +59,15 @@ if __name__ == "__main__":
     print(main(text))
 
 class Tests(unittest.TestCase):
-    text = '''LLR
+    text = '''LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)'''
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)'''
     def test(self):
         self.assertEqual(6, main(self.text))
