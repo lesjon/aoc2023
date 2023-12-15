2658bf77e442b79d739bfe00b0375c23873d4622
Leon Klute <leonklute@live.nl>
2023-12-16 00:17:40 +0100

    succes

--- main.py
+++ main.py
@@ -12,9 +12,46 @@ def _hash(s: str) -> int:
         current %= 256
     return current
 
+
+def execute(instr: str, boxes: list[list[str]], lenses: dict[str,int]):
+    if instr.endswith('-'):
+        id = instr.rstrip('-')
+        i = _hash(id)
+        l = boxes[i]
+        if id in l:
+            l.remove(id)
+            lenses.pop(id)
+    elif '=' in instr:
+        id, f = instr.split('=')
+        lenses[id] = int(f)
+        i = _hash(id)
+        box = boxes[i]
+        if not id in box:
+            box.append(id)
+    else:
+        raise Exception(f"Unknown execution '{instr}'")
+
+
 def run(text: str) -> int:
     parts = parse(text)
-    return sum(map(_hash, parts))
+    boxes = [[] for _ in range(256)]
+    lenses: dict[str, int] = {}
+    for instr in parts:
+        execute(instr, boxes, lenses)
+
+    focal_power = 0
+    for i, box in enumerate(boxes):
+        for j, lens_id in enumerate(box):
+            focal_power += (1+i) * (1+j) * lenses[lens_id]
+    return focal_power
+
+
+# To confirm that all of the lenses are installed correctly, add up the focusing power of all of the lenses. The focusing power of a single lens is the result of multiplying together:
+#     One plus the box number of the lens in question.
+#     The slot number of the lens within the box: 1 for the first lens, 2 for the second lens, and so on.
+#     The focal length of the lens.
+
+
 
 if __name__ == "__main__":
     with open('input.txt') as f:
@@ -23,55 +60,4 @@ if __name__ == "__main__":
 class Tests(unittest.TestCase):
     def test(self):
         text = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7\n'
-        self.assertEqual(1320, run(text))
-
-    def test_hash(self):
-        text = 'HASH'
-        self.assertEqual(52, _hash(text))
-
-    def test_rn(self):
-        text = '''rn=1'''
-        self.assertEqual(30, run(text))
-
-    def test_cm(self):
-        text = '''cm-'''
-        self.assertEqual(253, run(text))
-        
-    def test_qp(self):
-        text = 'qp=3'
-        self.assertEqual(97, run(text))
-
-    def test_cm2(self):
-        text = 'cm=2'
-        self.assertEqual(47, run(text))
-
-    def test_qpminus(self):
-        text = 'qp-'
-        self.assertEqual(14, run(text))
-
-    def test_pc4(self):
-        text = 'pc=4'
-        self.assertEqual(180, run(text))
-
-    def test_ot9(self):
-        text = 'ot=9'
-        self.assertEqual( 9, run(text))
-
-    def test_ab(self):
-        text = 'ab=5'
-        self.assertEqual( 197, run(text))
-
-    def test_pcminus(self):
-        text = 'pc-'
-        self.assertEqual( 48, run(text))
-
-    def test_pc6(self):
-        text = 'pc=6'
-        self.assertEqual( 214, run(text))
-
-    def test_ot7(self):
-        text = 'ot=7'
-        self.assertEqual( 231, run(text))
-
-
-
+        self.assertEqual(145, run(text))
