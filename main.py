0aef12c1220c65e484a4f02d8e45d3c1671821aa
Leon Klute <leonklute@live.nl>
2023-12-16 09:45:56 +0100

    succes

--- main.py
+++ main.py
@@ -1,56 +1,77 @@
 import unittest
 
 def parse(text: str) -> list[str]:
-    text = text.replace('\n', '')
-    return text.split(',')
-
-def _hash(s: str) -> int:
-    current = 0
-    for c in s:
-        current += ord(c)
-        current *= 17
-        current %= 256
-    return current
-
-
-def execute(instr: str, boxes: list[list[str]], lenses: dict[str,int]):
-    if instr.endswith('-'):
-        id = instr.rstrip('-')
-        i = _hash(id)
-        l = boxes[i]
-        if id in l:
-            l.remove(id)
-            lenses.pop(id)
-    elif '=' in instr:
-        id, f = instr.split('=')
-        lenses[id] = int(f)
-        i = _hash(id)
-        box = boxes[i]
-        if not id in box:
-            box.append(id)
-    else:
-        raise Exception(f"Unknown execution '{instr}'")
-
+    return text.splitlines()
 
 def run(text: str) -> int:
-    parts = parse(text)
-    boxes = [[] for _ in range(256)]
-    lenses: dict[str, int] = {}
-    for instr in parts:
-        execute(instr, boxes, lenses)
+    cave = parse(text)
+    width = len(cave[0])
+    height = len(cave)
+    lasers = {(0,0,1,0)}
+    seen_positions = set()
+    seen_lasers = set()
+    while lasers:
+        to_remove = []
+        lasers_to_parse = list(lasers) 
+        lasers.clear()
+        for x,y,vx,vy in lasers_to_parse:
+            seen_positions.add((x,y))
+            seen_lasers.add((x,y,vx,vy))
+            match cave[y][x]:
+                case '.':
+                    y += vy
+                    x += vx
+                    lasers.add((x,y,vx,vy))
+                case '-':
+                    if vy != 0:
+                        lasers.add((x+1,y, 1,0))
+                        lasers.add((x-1,y,-1,0))
+                    else:
+                        lasers.add((x+vx,y,vx,vy))
+                case '|':
+                    if vx != 0:
+                        lasers.add((x,y+1,0, 1))
+                        lasers.add((x,y-1,0,-1))
+                    else:
+                        lasers.add((x,y+vy,vx,vy))
+                case '\\':
+                    if vx == 1:
+                        lasers.add((x,y+1,0, 1))
+                    elif vx == -1:
+                        lasers.add((x,y-1,0,-1))
+                    elif vy == 1:
+                        lasers.add((x+1,y,1,0))
+                    elif vy == -1:
+                        lasers.add((x-1,y,-1, 0))
+                case '/':
+                    if vx == 1:
+                        lasers.add((x,y-1,0,-1))
+                    elif vx == -1:
+                        lasers.add((x,y+1,0, 1))
+                    elif vy == 1:
+                        lasers.add((x-1,y,-1, 0))
+                    elif vy == -1:
+                        lasers.add((x+1,y,1,0))
+        # for y in range(height):
+        #     for x in range(width):
+        #         if (x,y) in seen_positions:
+        #             print('#', end='')
+        #         else:
+        #             print(cave[y][x], end='')
+        #     print()
+        # print()
 
-    focal_power = 0
-    for i, box in enumerate(boxes):
-        for j, lens_id in enumerate(box):
-            focal_power += (1+i) * (1+j) * lenses[lens_id]
-    return focal_power
+        lasers = {l for l in lasers if 0 <= l[0] < width and 0 <= l[1] < height}
+        lasers -= seen_lasers
+    for y in range(height):
+        for x in range(width):
+            if (x,y) in seen_positions:
+                print('#', end='')
+            else:
+                print(cave[y][x], end='')
+        print()
 
-
-# To confirm that all of the lenses are installed correctly, add up the focusing power of all of the lenses. The focusing power of a single lens is the result of multiplying together:
-#     One plus the box number of the lens in question.
-#     The slot number of the lens within the box: 1 for the first lens, 2 for the second lens, and so on.
-#     The focal length of the lens.
-
+    return len(seen_positions)
 
 
 if __name__ == "__main__":
@@ -59,5 +80,14 @@ if __name__ == "__main__":
 
 class Tests(unittest.TestCase):
     def test(self):
-        text = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7\n'
-        self.assertEqual(145, run(text))
+        text = r'''.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....'''
+        self.assertEqual(46, run(text))
