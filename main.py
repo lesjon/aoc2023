9958b701e96602289de390096e67ff28b4c9fcdf
Leon Klute <leonklute@live.nl>
2023-12-16 10:08:15 +0100

    succes

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ import unittest
 def parse(text: str) -> list[str]:
     return text.splitlines()
 
-def run(text: str) -> int:
-    cave = parse(text)
-    width = len(cave[0])
-    height = len(cave)
-    lasers = {(0,0,1,0)}
+def energized_from(laser: tuple[int,int,int,int], width: int, height: int, cave: list[str]) -> int:
+    lasers = {laser}
     seen_positions = set()
     seen_lasers = set()
     while lasers:
-        to_remove = []
         lasers_to_parse = list(lasers) 
         lasers.clear()
         for x,y,vx,vy in lasers_to_parse:
@@ -63,15 +59,29 @@ def run(text: str) -> int:
 
         lasers = {l for l in lasers if 0 <= l[0] < width and 0 <= l[1] < height}
         lasers -= seen_lasers
-    for y in range(height):
-        for x in range(width):
-            if (x,y) in seen_positions:
-                print('#', end='')
-            else:
-                print(cave[y][x], end='')
-        print()
-
     return len(seen_positions)
+
+
+def run(text: str) -> int:
+    cave = parse(text)
+    width = len(cave[0])
+    height = len(cave)
+    starts = set()
+    for x in range(width):
+        starts.add((x,0,1,0))
+        starts.add((x,0,-1,0))
+        starts.add((x,0,0,1))
+        starts.add((x,height-1,1,0))
+        starts.add((x,height-1,-1,0))
+        starts.add((x,height-1,0,-1))
+    for y in range(height):
+        starts.add((0,y,1,0))
+        starts.add((0,y,0,-1))
+        starts.add((0,y,0,1))
+        starts.add((width-1,y,-1,0))
+        starts.add((width-1,y,0, 1))
+        starts.add((width-1,y,0,-1))
+    return max(*map(lambda l: energized_from(l, width, height, cave), starts))
 
 
 if __name__ == "__main__":
