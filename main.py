2c4066678767f3965706c0dae6319a4d14edc2e2
Leon Klute <leonklute@live.nl>
2023-12-18 13:09:28 +0100

    wip

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import unittest
 
 
+PART2 = False
+
 def parse(text: str) -> list[tuple[range, range]]:
     result = []
     for line in text.splitlines():
-        match line.split():
-            case ('D', steps, color):
-                result.append((range(0),range(int(steps))))
-            case ('R', steps, color):
-                result.append((range(int(steps)),range(0)))
-            case ('U', steps, color):
-                result.append((range(0),range(-int(steps),0)))
-            case ('L', steps, color):
-                result.append((range(-int(steps), 0),range(0)))
+        dir, steps, color = line.split()
+        if PART2:
+            match (color[2:7], color[7]):
+                case (steps, '1'):
+                    steps = int(float.fromhex(steps))
+                    result.append((range(0),range(steps)))
+                case (steps, '0'):
+                    steps = int(float.fromhex(steps))
+                    result.append((range(steps),range(0)))
+                case (steps, '3'):
+                    steps = int(float.fromhex(steps))
+                    result.append((range(0),range(-steps,0)))
+                case (steps, '2'):
+                    steps = int(float.fromhex(steps))
+                    result.append((range(-steps, 0),range(0)))
+        else:
+            match (steps, dir):
+                case (steps, 'D'):
+                    result.append((range(0),range(int(float.fromhex(steps)))))
+                case (steps, 'R'):
+                    result.append((range(int(float.fromhex(steps))),range(0)))
+                case (steps, 'U'):
+                    result.append((range(0),range(-int(float.fromhex(steps)),0)))
+                case (steps, 'L'):
+                    result.append((range(-int(float.fromhex(steps)), 0),range(0)))
     return result
-    
-
 
 
 
@@ -34,53 +50,57 @@ def main(text: str) -> int:
     path.update({current: (0,0)})
     for (x_range,y_range) in instrs:
         print(f'{x_range} {y_range}')
-        for y in y_range:
-            path.update({current:(0, sign(y))})
+        y_length = len(y_range)
+        for i, y in enumerate(y_range):
+            if i == 0:
+                path[current] = (0, sign(y))
             current = (current[0], current[1] + sign(y))
-            path.update({current:(0, sign(y))})
+            if i+1 == y_length:
+                path[current] = (0, sign(y))
+            else:
+                path[current] = (0, 2*sign(y))
             min_y = min(min_y, current[1])
             max_y = max(max_y, current[1])
         for x in x_range:
             current = (current[0] + sign(x), current[1])
-            path.update({current:(sign(x), 0)})
+            if not current in path:
+                path.update({current:(sign(x), 0)})
             min_x = min(min_x, current[0])
             max_x = max(max_x, current[0])
     print(f'{min_x=}-{max_x=} {min_y=}-{max_y=}')
-    print(path)
+    print(f'{len(path)=}')
     inners = set()
     for y in range(min_y, max_y+1):
+        inside = False
+        in_trench = 0
         for x in range(min_x, max_x+1):
             if (x,y) in path:
-                match path.get((x,y)):
-                    case (0,1):
-                        print('v', end='')
-                    case (0,-1):
-                        print('^', end='')
-                    case (1,0):
-                        print('>', end='')
-                    case (-1,0):
-                        print('<', end='')
+                _,y_dir = path.get((x,y))
+                print(abs(y_dir), end='')
+                # match y_dir:
+                #     case 1 | 2:
+                #         if not PART2:
+                #             print('v', end='')
+                #     case -1 | -2:
+                #         if not PART2:
+                #             print('^', end='')
+                #     case other:
+                #         if not PART2:
+                #             print(other, end='')
+                in_trench += y_dir
             else:
-                lefts_coords = ((check_x, y) for check_x in range(min_x, x))
-                crossings = 0
-                in_trench = 0
-                for coord in lefts_coords:
-                    if coord in path:
-                        (_,y_dir) = path.get(coord)
-                        in_trench += y_dir
-                    else:
-                        if in_trench != 0:
-                            crossings += 1
-                        in_trench = 0
-                if in_trench != 0:
-                    crossings += 1
-
-                if crossings % 2 == 1:
+                if (in_trench // 2 ) % 2 == 1:
+                    inside = not inside
+                    in_trench = 0
+                if inside:
                     inners.add((x,y))
-                    print('I', end='')
+                    if not PART2:
+                        print('I', end='')
                 else:
-                    print('.', end='')
-        print()
+                    if not PART2:
+                        print('.', end='')
+        if not PART2:
+            print()
 
     return len(path) + len(inners)
 
@@ -107,6 +127,6 @@ U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)
 '''
-        self.assertEqual(62, main(text))
+        self.assertEqual(952408144115, main(text))
     
     
