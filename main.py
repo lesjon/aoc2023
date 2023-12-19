725d84ff4a162a15d4885d8730873392d14535de
Leon Klute <leonklute@live.nl>
2023-12-19 17:36:36 +0100

    succes

--- main.py
+++ main.py
@@ -1,103 +1,66 @@
 import unittest
+import json
+
+def parse_instr(workflow: str):
+    label, steps = workflow.split('{')
+    steps = steps[:-1]
+    steps = steps.split(',')
+    instrs = []
+    for step in steps:
+        if ':' in step:
+            comparison, target = step.split(':')
+            if '>' in comparison:
+                key, value = comparison.split('>')
+                comparison = lambda item, k=key, i=int(value): item[k] > i
+            elif '<' in comparison:
+                key, value = comparison.split('<')
+                comparison = lambda item, k=key, i=int(value): item[k] < i
+            else:
+                raise
+        else:
+            comparison, target = lambda i: True, step
+        instrs.append((comparison, target))
+
+    return label, instrs
+
+def parse_item(item: str):
+    item = item.replace('x', '"x"')
+    item = item.replace('a', '"a"')
+    item = item.replace('m', '"m"')
+    item = item.replace('s', '"s"')
+    item = item.replace('=', ':')
+    return json.loads(item)
 
 
-X_KEY = None
+def parse(text: str) -> tuple:
+    workflows, items = map(str.splitlines, text.split('\n\n'))
+    workflows = map(parse_instr, workflows)
+    items = map(parse_item, items)
+    return workflows, items
 
-def parse_steps(steps: str) -> int:
-    return int(float.fromhex(steps))
-
-def parse(text: str) -> tuple[dict[int, list[range]],int,int, dict[tuple[int,int], int], list[int]]:
-    min_y = max_y = 0
-    trenches = dict()
-    trenches[X_KEY] = []
-    corners = dict()
-    current = [0,0]
-    columns = {current[0]}
-    for line in text.splitlines():
-        dir, steps, color = line.split()
-        match (color[2:7], color[7]):
-            case (steps, '0'):
-                steps = parse_steps(steps)
-                trenches[None].append(range(current[0], current[0]+steps))
-                current[0] += steps
-                columns.add(current[0])
-            case (steps, '1'):
-                corners[tuple(current)] = 1
-                steps = parse_steps(steps)
-                if current[0] in trenches:
-                    trenches[current[0]].append(range(current[1], current[1]+steps))
-                else:
-                    trenches[current[0]] = [range(current[1], current[1]+steps)]
-                current[1] += steps
-                corners[tuple(current)] = 1
-                max_y = max(max_y, current[1])
-            case (steps, '3'):
-                corners[tuple(current)] = -1
-                steps = parse_steps(steps)
-                if current[0] in trenches:
-                    trenches[current[0]].append(range(current[1]-steps, current[1]))
-                else:
-                    trenches[current[0]] = [range(current[1]-steps, current[1])]
-                current[1] -= steps
-                corners[tuple(current)] = -1
-                min_y = min(min_y, current[1])
-            case (steps, '2'):
-                steps = parse_steps(steps)
-                trenches[None].append(range(current[0]-steps, current[0]))
-                current[0] -= steps
-                columns.add(current[0])
-            case other:
-                raise Exception(f'Unknown directions {other}')
-    columns = sorted(columns)
-    return trenches, min_y , max_y, corners, columns
-
-
-def sign(num: int) -> int:
-    if num < 0:
-        return -1
-    elif num >= 0:
-        return 1
-    return 0
 
 def main(text: str) -> int:
-    trenches, min_y , max_y, corners, columns = parse(text)
-    
-    print(f'y_range:{min_y} - {max_y}')
-    print(f'{len(trenches)=}')
-    print(f'{trenches=}')
-    print(f'{corners=}')
-    print(f'{columns=}')
-    inners = 0 
-    y_range = range(min_y, max_y+1)
-    for i_y, y in enumerate(y_range):
-        print(i_y/len(y_range))
-        crossings = 0
-        half_crossings = 0
-        for i_x, x in enumerate(columns[:-1]):
-            print(f'{(x,y)=}')
-            next_x = columns[i_x+1]
-            columns_trenches = trenches[x]
-            if (x,y) in corners:
-                half_crossings += corners[(x,y)]
-                print(f'{half_crossings=} {crossings=}')
-                if half_crossings % 2 == 0:
-                    half_crossings = 0
-                    crossings += 1
-            elif any(y in trench for trench in columns_trenches):
-                crossings += 1
-            if crossings % 2 == 1:
-                inners += next_x - x
-                print(f'increased {inners=}')
-        print(f' {crossings=}')
-        raise
+    workflows, items = parse(text)
+    workflows = dict(workflows)
+    items = list(items)
+    print(f'{workflows=}\n{items=}')
+    total = 0
+    END_STATES = {'R', 'A'}
+    for i in items:
+        print(f'{i=}')
+        current = 'in'
+        while not current in END_STATES:
+            print(f'{current=}')
+            workflow = workflows[current]
+            for step in workflow:
+                print(f'{step=}')
+                if step[0](i):
+                    current = step[1]
+                    break
+        if current == 'A':
+            total+= sum(i.values())
 
-    print(f'{trenches=}')
-    trench_lens = 0
-    for l in trenches.values():
-        trench_lens += sum(map(len, l))
-    print(f'{trench_lens =}')
-
-    return trench_lens + inners
+    return total
 
 
 if __name__ == "__main__":
@@ -106,35 +69,24 @@ if __name__ == "__main__":
     print(main(text))
 
 class Tests(unittest.TestCase):
-# 0 means R, 1 means D, 2 means L, and 3 means U.
-#     def test(self):
-#         text = '''R 6 (#000060)
-# R 6 (#000043)
-# R 6 (#000022)
-# R 6 (#000021)
-# R 6 (#000022)
-# R 6 (#000023)
-# R 6 (#000022)
-# R 6 (#000041)
-# '''
-        # self.assertEqual(25, main(text))
 
     def test(self):
-        text = '''R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)
+        text = '''px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
 '''
-        self.assertEqual(952408144115, main(text))
-    
-    
+        self.assertEqual(19114, main(text))
