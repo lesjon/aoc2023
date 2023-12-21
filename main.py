228f8d1278d389c47775aae28014902cd9065325
Leon Klute <leonklute@live.nl>
2023-12-21 21:53:06 +0100

    succes

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import unittest
 from typing import Optional
 
-def flip_flop(state: tuple[str, list[str], dict[str, int]| int], signal: int) -> tuple[tuple[str, list[str], dict[str, int] | int], Optional[int]]:
+class Module:
+    def __init__(self, name: str, version: str, targets: list[str], state: None | dict[str,int] | int) -> None:
+        self.name = name
+        self.targets = targets
+        self.state = state
+        self.version = version
+
+    def __repr__(self) -> str:
+        return f'{self.version}{self.name} [ {self.state} ] -> {self.targets}'
+
+def flip_flop(module: Module, signal: int) -> tuple[Module, Optional[int]]:
 # Flip-flop modules (prefix %) are either on or off; they are initially off. If a flip-flop module receives a high pulse, it is ignored and nothing happens. However, if a flip-flop module receives a low pulse, it flips between on and off. If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
-    assert state[0] == '%'
-    assert isinstance(state[2], int)
+    assert module.version == '%'
+    assert isinstance(module.state, int)
     if signal == 1:
-        return state, None
+        return module, None
     elif signal == 0:
-        return (*state[:2], 1-state[2]), 1-state[2]
+        module.state = 1-module.state
+        return module, module.state
     raise
 
-def conjunction(state: tuple[str, list[str], dict[str, int]| int], signal: tuple[str, int]) -> tuple[tuple[str, list[str], dict[str, int]| int], int]:
+def conjunction(module: Module, signal: tuple[str, int]) -> tuple[Module, int]:
 # Conjunction modules (prefix &) remember the type of the most recent pulse received from each of their connected input modules; they initially default to remembering a low pulse for each input. When a pulse is received, the conjunction module first updates its memory for that input. Then, if it remembers high pulses for all inputs, it sends a low pulse; otherwise, it sends a high pulse.
-    assert state[0] == '&'
-    assert isinstance(state[2], dict)
-    state[2][signal[0]] = signal[1]
-    return state, 1 if sum(state[2].values()) == len(state[2]) else 0
+    assert module.version == '&'
+    assert isinstance(module.state, dict)
+    module.state[signal[0]] = signal[1]
+    return module, 0 if sum(module.state.values()) == len(module.state) else 1
 
 
-def parse(text: str) -> dict[str, tuple[str, list[str], dict[str, int]| int]]:
+def parse(text: str) -> tuple[dict[str, Module], set[str]]:
     modules = {}
+    outputs = set()
     connections = []
     for line in text.splitlines():
         match line.split():
@@ -28,33 +40,37 @@ def parse(text: str) -> dict[str, tuple[str, list[str], dict[str, int]| int]]:
                 targets = list(map(lambda t: t.rstrip(',') , targets))
                 for t in targets:
                     connections.append(('broadcaster', t))
-                modules['broadcaster'] = (None, targets, None)
+                modules['broadcaster'] = Module('broadcaster', "", targets, None)
             case (id, '->', *targets):
                 ty, label = id[0], id[1:]
                 targets = list(map(lambda t: t.rstrip(',') , targets))
                 for t in targets:
                     connections.append((label, t))
                 state = dict() if ty == '&' else 0
-                modules[label] = (ty, targets, state)
+                modules[label] = Module(label, ty, targets, state)
             case other:
                 raise Exception(f'Parse error: {other}')
-    print(f'{connections=}')
-    print(f'{modules=}')
+    # print(f'{connections=}')
+    # print(f'{modules=}')
     for label, target in connections:
-        if target == 'output':
+        if target in outputs:
             continue
-        if isinstance( modules[target][2], dict):
-            modules[target][2][label] = 0
-    return modules
+        if target in modules:
+            if isinstance( modules[target].state, dict):
+                modules[target].state[label] = 0
+        else:
+            outputs.add(target)
+    return modules, outputs
 
 def main(text: str, presses: int) -> int:
-    modules = parse(text)
+    modules, outputs = parse(text)
     low_pulses = high_pulses = 0
     print(f'{modules=}')
     for i in range(presses):
         print('for', i)
+        # button press
+        low_pulses += 1
         signals: list[tuple[str, int]] = [('broadcaster', 0)]
-        print('while')
         while signals:
             signal = signals.pop(0)
 # button -low-> broadcaster
@@ -65,45 +81,29 @@ def main(text: str, presses: int) -> int:
 # con -high-> output
 # b -high-> con
 # con -low-> output
-            print(f'{signal=}')
-            module = modules[signal[0]]
-            print(f'\n{module=} -{{', end='')
-            match module[0]:
-                case '&':
-                    modules[signal[0]], output = conjunction(module, signal)
-                    for t in module[1]:
-                        if output == 0:
-                            low_pulses += 1
-                        else:
-                            high_pulses += 1
-                        if t != 'output':
-                            print(f'{output=}}}-> {t}')
-                            signals.append((t, output))
-                case '%':
-                    modules[signal[0]], output = flip_flop(module, signal[1])
-                    if not output is None:
-                        for t in module[1]:
-                            if t == 0:
-                                low_pulses += 1
-                            else:
-                                high_pulses += 1
-                            if t != 'output':
-                                signals.append((t, output))
-                    print(output)
-                case None:
-                    output = signal[1]
-                    for t in module[1]:
-                        if output == 0:
-                            low_pulses += 1
-                        else:
-                            high_pulses += 1
-                        if t != 'output':
-                            print(f'{output=}}}-> {t}')
-                            signals.append((t, output))
-                case other:
-                    raise Exception(f'Unknown module: {other}')
-            print(f'{modules=}')
-        print(modules)
+            # print(f'{signal=}')
+            source_module = modules[signal[0]]
+            # print(f'\n{signal[0]} -[{signal[1]}]', end='')
+            for t in source_module.targets:
+                # print(f'\t-> {t}')
+                # print(f'{signal[1]=} {low_pulses=} {high_pulses=}') 
+                low_pulses += 1 - signal[1]
+                high_pulses += signal[1]
+                if t in outputs:
+                    continue
+                output = None
+                target_module = modules[t]
+                match target_module.version:
+                    case '&':
+                        modules[t], output = conjunction(target_module, signal)
+                    case '%':
+                        modules[t], output = flip_flop(target_module, signal[1])
+                    case '':
+                        output = signal[1]
+                if output is not None:
+                    signals.append((t, output))
+            # print(f'{modules=}')
+        # print(modules)
     print(low_pulses, high_pulses)
     return low_pulses * high_pulses
 
@@ -114,9 +114,17 @@ def main(text: str, presses: int) -> int:
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
         text = f.read()
-    print(main(text), 1000)
+    print(main(text, 1000))
 
 class Tests(unittest.TestCase):
+    def test_short(self):
+        text = '''broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a'''
+        self.assertEqual(4*8, main(text, 1))
+
     def test(self):
         text = '''broadcaster -> a
 %a -> inv, con
